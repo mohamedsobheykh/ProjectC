9b6504ef2fadab56744f25494c48c73a1cd5e24e
Mohamed Sobhey <mohamedsobhey@zewailcity.edu.eg>
2020-11-28 19:20:45 +0200

    feature #7 added

---
+++ SPOT/Registrar.cpp
@@ -0,0 +1,99 @@
+#include "Registrar.h"
+
+#include "Actions/ActionAddCourse.h"
+
+
+Registrar::Registrar()
+{
+	pGUI = new GUI;	//create interface object
+	pSPlan = new StudyPlan;	//create a study plan.
+}
+
+//returns a pointer to GUI
+GUI* Registrar::getGUI() const
+{
+	return pGUI;
+}
+
+//returns the study plan
+StudyPlan* Registrar::getStudyPlay() const
+{
+	return pSPlan;
+}
+
+bool Registrar::saveStudyPlan(string name)
+{
+	string directory = "savedplans\\" + name + ".txt";
+	fstream* pFile = new fstream (directory, ios::out);
+
+	if (!*pFile) // checks that the file is opened successfully
+	{
+		return false;
+	}
+
+	pSPlan->SaveMe(pFile);
+
+	(*pFile).close();
+	delete pFile;
+	return true;
+}
+
+Action* Registrar::CreateRequiredAction() 
+{	
+	ActionData actData = pGUI->GetUserAction("Pick and action...");
+	Action* RequiredAction = nullptr;
+
+	switch (actData.actType)
+	{
+	case ADD_CRS:	//add_course action
+		RequiredAction = new ActionAddCourse(this);
+		break;
+
+	//TODO: Add case for each action
+	
+	case EXIT:
+		break;
+		
+	}
+	return RequiredAction;
+}
+
+//Executes the action, Releases its memory, and return true if done, false if cancelled
+bool Registrar::ExecuteAction(Action* pAct)
+{
+	bool done = pAct->Execute();
+	delete pAct;	//free memory of that action object (either action is exec or cancelled)
+	return done;
+}
+
+void Registrar::Run()
+{
+	while (true)
+	{
+		//update interface here as CMU Lib doesn't refresh itself
+		//when window is minimized then restored
+		UpdateInterface();
+
+		Action* pAct = CreateRequiredAction();
+		if (pAct)	//if user doesn't cancel
+		{
+			if (ExecuteAction(pAct))	//if action is not cancelled
+				UpdateInterface();
+		}
+		else
+			break;
+		
+	}
+}
+
+
+void Registrar::UpdateInterface()
+{
+	pGUI->UpdateInterface();	//update interface items
+	pSPlan->DrawMe(pGUI);		//make study plan draw itself
+}
+
+Registrar::~Registrar()
+{
+	delete pGUI;
+}
---
+++ SPOT/Registrar.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "GUI\GUI.h"
+#include "Actions\Action.h"
+#include "Rules.h"
+#include "StudyPlan/StudyPlan.h"
+#include <string>
+#include <fstream>
+
+
+//The maestro class for the application
+class Registrar
+{
+	GUI *pGUI;	//pointer to GUI 
+	Rules RegRules;	//Registration rules
+	StudyPlan *pSPlan;
+
+public:
+	Registrar();
+	GUI* getGUI() const;
+	Action* CreateRequiredAction();
+	bool ExecuteAction(Action*);
+	void UpdateInterface();
+	StudyPlan* getStudyPlay() const;
+	bool saveStudyPlan(string name);
+
+	void Run();
+
+	~Registrar();
+};
+
